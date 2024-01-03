64e08c82f3cb8a8a8c6037194ad01a68ecfa92b4
SoufianeAbo <abounasrsoufiane@gmail.com>
2024-01-03 11:29:58 +0100

    finish dashboard

--- View/dashboardUser.php
+++ View/dashboardUser.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -97,17 +100,9 @@
 
     <aside class="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
         <div class="p-6">
-            <a class="text-white text-3xl font-semibold uppercase hover:text-gray-300"><img src="./img/white3.png" alt=""></a>
+            <a class="text-white text-3xl font-semibold uppercase hover:text-gray-300"><img src="../img/white3.png" alt=""></a>
         </div>
         <nav class="text-white text-base font-semibold pt-3">
-            <a class="flex items-center active-nav-link text-white py-4 pl-6 nav-item cursor-pointer" id = "TeamsBtn">
-                <i class="fa-solid fa-users mr-3"></i>
-                Teams
-            </a>
-            <a class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item cursor-pointer" id = "MembersBtn">
-                <i class="fa-solid fa-user-group mr-3"></i>
-                Members
-            </a>
             <a class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item cursor-pointer" id = "ProjectsBtn">
                 <i class="fa-solid fa-list-check mr-3"></i>
                 Projects
@@ -147,14 +142,6 @@
             </div>
 
             <nav :class="isOpen ? 'flex': 'hidden'" class="flex flex-col pt-4">
-                <a class="flex items-center active-nav-link text-white py-2 pl-4 nav-item" id = "TeamsBtn2">
-                    <i class="fa-solid fa-users mr-3"></i>
-                    Teams
-                </a>
-                <a class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item" id = "MembersBtn2">
-                    <i class="fa-solid fa-user-group mr-3"></i>
-                    Members
-                </a>
                 <a class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item" id = "ProjectsBtn2">
                     <i class="fa-solid fa-list-check mr-3"></i>
                     Projects
@@ -166,100 +153,6 @@
         </header>
     
         <div class="w-full overflow-x-hidden border-t flex flex-col">
-            <main class="w-full grid grid-flow-row grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-6" id = "TeamsTable">
-                <h1 class="text-3xl text-black pb-6 col-span-3">Your teams</h1>
-
-                <?php
-                   $teams = $teamObj->getTeamDetailsBySession();
-
-                   foreach ($teams as $team) {
-                       $scrumMasterDetails = $teamObj->getScrumMasterDetails($team['scrumMasterID']);
-           
-                       echo '<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">';
-                       echo '<a href="#">';
-                       echo "<img class='rounded-t-lg' src='{$team['image']}' alt='' />";
-                       echo '</a>';
-                       echo '<div class="p-5">';
-                       echo '<div class="flex justify-between">';
-                       echo '<a href="#" class="flex flex-col">';
-                       echo "<h5 class='text-2xl font-bold tracking-tight text-gray-900'>{$team['teamName']}</h5>";
-                       echo "<p class='mb-4 text-green-900'><i class='fa-solid fa-user-pen pr-2'></i>{$scrumMasterDetails['firstName']} {$scrumMasterDetails['lastName']}</p>";
-                       echo '</a>';
-                       echo '';
-                       echo "<img src='{$scrumMasterDetails['image']}' alt='' class='w-[14%] h-[14%] rounded-full border-2 border-green-700 relative'>";
-                       echo '</div>';
-                       echo "<p class='mb-3 font-normal text-gray-700'>{$team['description']}</p>";
-                       echo '<div class="flex flex-row items-center justify-between">';
-                       echo '<div>';
-                       echo '<a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">';
-                       echo 'View members';
-                       echo '<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">';
-                       echo '<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>';
-                       echo '</a>';
-                       echo '</svg>';
-                       echo '</div>';
-                       echo '<div class="flex flex-col">';
-                       echo '</div>';
-                       echo '</div>';
-                       echo '</div>';
-                       echo '</div>';
-                   }
-                ?>
-            </main>
-
-            <main class="w-full grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-5 p-6 justify-center items-center hidden gap-5" id = "MembersTable">
-                <h1 class="text-3xl text-black pb-6 col-span-1 md:col-span-2 lg:col-span-5">Members</h1>
-                <?php
-                    $equipeID = $_SESSION['equipeID'];
-                    $teamDetails = $teamObj->getTeamById($equipeID);
-
-                    foreach ($teamDetails as $team) {
-                        $teamImg = $team['image'];
-                        $teamName = $team['teamName'];
-                        $scrumMasterID = $team['scrumMasterID'];
-                        $teamId = $team['id'];
-
-                        echo '
-                            <div class="w-full h-48 col-span-1 md:col-span-2 lg:col-span-5 bg-white border border-gray-200 rounded-lg shadow sticky top-0" style = "background-image: url(' . $teamImg . '); background-position-x: center; background-position-y: 20%; background-repeat: no-repeat; background-size: cover;">
-                                <div class = "w-full h-fit bg-gray-800 py-2 rounded-t">
-                                    <p class = "text-white text-center">' . $teamName . '</p>
-                                </div>
-                            </div>';
-
-                            $teamMembers = $userObj->getAllUsersFromTeam($teamId);
-
-                            foreach ($teamMembers as $member) {
-                                $MembersFirstName = $member['firstName'];
-                                $MembersLastName = $member['lastName'];
-                                $MembersImg = $member['image'];
-                                $MembersRole = $member['role'];
-                                $MembersColor = $userObj->getRoleColor($MembersRole);
-                                $MembersIcon = $userObj->getRoleIcon($MembersRole);
-                                $MembersRole = $userObj->getRoleName($MembersRole);
-                            echo '
-                                <div class="w-full max-w-sm bg-white border border-gray-100 rounded-lg shadow">
-                                    <div class="flex flex-col items-center pb-2">
-                                        <div class = "flex flex-row justify-between px-2 py-2 mb-2 bg-gray-800 rounded-t border border-gray-100">
-                                            <p class = "text-white font-bold"><i class="fa-solid fa-flag mr-2"></i>' . $teamName . '</p>
-                                            <img src="' . $teamImg . '" alt="" class = "rounded-full h-1/6 w-1/6">
-                                        </div>
-                                        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="' . $MembersImg . '" alt="' . $MembersFirstName . ' ' . $MembersLastName . '"/>
-                                        <h5 class="mb-1 text-xl font-medium text-'. $MembersColor .'-900">' . $MembersFirstName . ' ' . $MembersLastName . '</h5>
-                                        <span class="text-sm text-'. $MembersColor .'-500"><i class="'. $MembersIcon .'"></i>'. $MembersRole .'</span>
-                                    </div>
-                                    <div class="flex pb-2 justify-center">
-                                    <form method = "POST" action = "remove.php">
-                                        <input type="hidden" name="userID" value="'. $member['id'] .'">
-                                    </form>
-                                    </div>
-                                </div>
-                                ';
-                        }
-                    }
-
-                ?>
-
-            </main>
 
             <main class="w-full grid grid-flow-row grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-6 hidden" id = "ProjectsTable">
                 <h1 class="text-3xl text-black pb-6 col-span-3">Your projects</h1>
